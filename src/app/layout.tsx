ea5a79189db3029040b718d0134369425db5ce12
novinbukannopin <jylovien@gmail.com>
2025-06-21 20:04:40 +0700

    feat: add ga4 with gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google"
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -68,6 +69,8 @@ export default function RootLayout({
           </TooltipProvider>
         </ThemeProvider>
       </body>
+    <GoogleAnalytics gaId={"G-W1MDRB9NRR"}/>
+    <GoogleTagManager gtmId={"G-W1MDRB9NRR"}/>
     </html>
   );
 }
